27c52444188dbc30303caed51e42f3c094044d7a
RUnroe <ryanunroe@gmail.com>
2022-01-11 08:40:56 -0700

    Set up initial db connection

--- backend/db/dal.js
+++ backend/db/dal.js
@@ -1,4 +1,14 @@
 const bcrypt = require('bcryptjs');
+// const firebase = require('firebase');
+// const admin = require("firebase-admin");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+
+initializeApp({credential: cert(require('../secrets').firebase)});
+const db = getFirestore();
+
 
 let salt = bcrypt.genSaltSync(10);
 
@@ -10,6 +20,5 @@ const hash = async (pw) => {
 		});
 };
 const verify_hash = (input, hash) => bcrypt.compareSync(input, hash);
-
 
 
