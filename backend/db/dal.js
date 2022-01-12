d5017525235effa066a142de2597dcc2dd6638c5
RUnroe <ryanunroe@gmail.com>
2022-01-11 17:37:17 -0700

    Get basic firebase connection setup

--- backend/db/dal.js
+++ backend/db/dal.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcryptjs');
-// const firebase = require('firebase');
-// const admin = require("firebase-admin");
+
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
@@ -22,3 +21,49 @@ const hash = async (pw) => {
 const verify_hash = (input, hash) => bcrypt.compareSync(input, hash);
 
 
+const isFieldEmpty = field => {
+	return (field === undefined || field === null);
+}
+const findErrors = fields => {
+	const errors = [];
+	fields.forEach(field => {
+		if(isFieldEmpty(field.value) || typeof field.value != (field.type ? field.type : "string") ||(field.regex && !field.regex.test(field.value))) {
+			errors.push(`Expected ${field.name}, but ${field.value} was supplied`);
+		}
+	});
+	return errors;
+}
+
+
+// ==============================
+//            Users
+// ==============================
+
+const createUser = async (_user) => {
+    //validate data
+    const errors = findErrors([
+		{name: "username", value: _user.username, regex: /^[a-zA-Z0-9_ ]+$/}, 
+		{name: "password", value: _user.password, regex: /^(?=.*[A-Za-z])(?=.*\d).{8,}$/}
+	]);
+	if (errors.length) {
+		throw errors;
+	}
+
+    //salt and hash password
+
+
+    //add user to database
+    const userId = genId();
+
+    const result = await db.collection('users').doc(userId).set({
+        username: _user.username,
+        password: securePassword,
+        is_admin: false,
+        api_key: genId(),
+        user_id: userId
+
+    });
+
+    //create session for user 
+
+}
--- backend/routes/api/movies.js
+++ backend/routes/api/movies.js
@@ -24,6 +24,15 @@ const getMoviesBySearch = (req, res) => {
 }
 
 
+//Get collection of movies
+//TODO: Get query params and pass through to dal
+const getMovies = (req, res) => {
+	dal.getMovies().then(result => {
+		res.json(result);
+	})
+	.catch(handle(req, res));
+}
+
 
 
 const routes = [
@@ -36,6 +45,12 @@ const routes = [
 		uri: '/api/search',
 		methods: ['get'],
 		handler: getMoviesBySearch
+	},
+	{
+		uri: '/api/movies',
+		methods: ['get'],
+		handler: getMovies
+
 	}
 ];
 
