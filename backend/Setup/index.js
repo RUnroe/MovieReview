84fbe2fb8bc3286659ea39ac5c61b4e25f2d86e9
RUnroe <ryanunroe@gmail.com>
2022-01-20 09:20:39 -0700

    Rework setup database methods

--- backend/Setup/index.js
+++ backend/Setup/index.js
@@ -3,54 +3,13 @@ const short = require('short-uuid');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-// const fs = require("fs");
-// csv = fs.readFileSync("users2-Group2.csv")
 
-// var array = csv.toString().split("\r");
-
-// let result = [];
- 
-// let headers = array[0].split(", ")
-
-// for (let i = 1; i < array.length - 1; i++) {
-//   let obj = {}
-
-//   let str = array[i]
-//   let s = ''
-
-//   let flag = 0
-//   for (let ch of str) {
-//     if (ch === '"' && flag === 0) {
-//       flag = 1
-//     }
-//     else if (ch === '"' && flag == 1) flag = 0
-//     if (ch === ', ' && flag === 0) ch = '|'
-//     if (ch !== '"') s += ch
-//   }
-
-//   let properties = s.split("|")
-
-//   for (let j in headers) {
-//     if (properties[j].includes(", ")) {
-//       obj[headers[j]] = properties[j]
-//         .split(", ").map(item => item.trim())
-//     }
-//     else obj[headers[j]] = properties[j]
-//   }
-
-//   result.push(obj)
-// }
-
-// let json = JSON.stringify(result);
-// fs.writeFileSync('output.json', json);
-
-initializeApp({credential: cert(require('./secrets.json').firebase)});
+initializeApp({credential: cert(require('../secrets.json').firebase)});
 const db = getFirestore();
-//console.log(db);
+// console.log(db);
 const fs = require("fs");
-const { errors } = require('jose');
 
-json = JSON.parse(fs.readFileSync("csvjson.json"));
+json = JSON.parse(fs.readFileSync("./csvjson.json"));
 
 const hash = (pw) => {
 	return  bcrypt.hashSync(pw, 8);
@@ -65,6 +24,8 @@ const isFieldEmpty = field => {
 const findErrors = fields => {
 	const errors = [];
 	fields.forEach(field => {
+        //make value a string if I can
+        if((!field.type || field.type === "string") && typeof field.value != "string") field.value = `${field.value}`; 
 		if(isFieldEmpty(field.value) || typeof field.value != (field.type ? field.type : "string") ||(field.regex && !field.regex.test(field.value))) {
 			errors.push(`Expected ${field.name}, but ${field.value} was supplied`);
 		}
@@ -73,14 +34,12 @@ const findErrors = fields => {
 }
 
 const createUser = async (_user) => {
-    _user.zip_code = `${_user.zip_code}`;
-    _user.phone = `${_user.phone}`;
-    _user.password = `${_user.password}`;
+
 
      //validate data
      const errors = findErrors([
 		{name: "email", value: _user.email, regex: /\w+@\w+\.\w+/}, 
-		{name: "password", value: _user.password, regex:   /^.{6,}$/},
+		{name: "password", value: _user.password, regex: /^.{6,}$/},
 		{name: "phone", value: _user.phone},
 		{name: "first name", value: _user.fname, regex:  /./}, 
 		{name: "last name", value: _user.lname, regex:  /./}, 
@@ -116,7 +75,7 @@ const createUser = async (_user) => {
         user_id: userId
 
     }).catch(e => console.log(e));
-    console.log(result);
+    // console.log(result);
     return {user_id: userId, is_admin: false};
 
 }
@@ -125,8 +84,8 @@ const PlzWork = async () => {
     for(let i = 0; i < json.length; i++){
         await createUser(json[i]).catch(errors => (console.log(errors)));
     }
+    console.log("finish");
 }
 
 PlzWork();
 //createUser(json[0])
-console.log("finish");
