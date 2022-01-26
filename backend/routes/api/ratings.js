88b53874607edfb49e193a55b862b483ae9d1dcf
RUnroe <ryanunroe@gmail.com>
2022-01-26 08:34:39 -0700

    Allow for ratings to be created through the API

--- backend/routes/api/ratings.js
+++ backend/routes/api/ratings.js
@@ -7,11 +7,20 @@ const configure = (obj) => {
 const { requireAuth, requireNotAuth, handle } = require('../util');
 
 const createRating = (req, res) => {
-	dal.createRating(req.session.user_id, req.params.movie_id, req.body.rating).then((success) => {
-		if(success) res.sendStatus(200);
-		else res.sendStatus(401);
-	})
-	.catch(handle(req, res));
+	if(req.query.api_key) {
+		dal.createRatingAPI(req.query.api_key, req.params.movie_id, req.body.rating).then((success) => {
+			if(success) res.sendStatus(200);
+			else res.sendStatus(401);
+		})
+		.catch(handle(req, res));
+	}
+	else {
+		dal.createRating(req.session.user_id, req.params.movie_id, req.body.rating).then((success) => {
+			if(success) res.sendStatus(200);
+			else res.sendStatus(401);
+		})
+		.catch(handle(req, res));
+	}
 }
 //get all ratings by for a movie
 const getRatings = (req, res) => {
