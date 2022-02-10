c6ffeb88c18a0a9208a431b8b3619007cb8f9deb
PhillipDuarte1 <60678196+PhillipDuarte1@users.noreply.github.com>
2022-02-10 08:31:51 -0700

    Setup Angular Routing and Navigation with a few components in

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details', component: MovieDetailsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  MovieDetailsComponent,
+  LoginComponent,
+  RegisterComponent
+]
