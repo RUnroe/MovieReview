d9daf0262a772df7221bbdc0bdcab5fa3fe242eb
RUnroe <ryanunroe@gmail.com>
2022-02-23 07:58:40 -0700

    Install apollo angular in front end

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -5,17 +5,35 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, ApolloModule, HttpClientModule, 
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [   
+    {
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        }),
+      };
+    },
+    deps: [HttpLink],
+  },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
