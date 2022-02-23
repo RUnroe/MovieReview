29724bfabd514ad770b17962d8ed27406b7a4d94
RUnroe <ryanunroe@gmail.com>
2022-02-23 07:59:46 -0700

    Install apollo angular in front end

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -28,7 +28,7 @@ import {InMemoryCache} from '@apollo/client/core';
       return {
         cache: new InMemoryCache(),
         link: httpLink.create({
-          uri: 'https://48p1r2roz4.sse.codesandbox.io',
+          uri: 'http://localhost:3005',
         }),
       };
     },
