19d25affd837c953adb180d64dbcb82074c5ce11
RUnroe <ryanunroe@gmail.com>
2022-03-02 08:43:44 -0700

    Fix JSX style syntax to match html syntax

--- frontend-angular/src/app/delete-account/delete-account.component.ts
+++ frontend-angular/src/app/delete-account/delete-account.component.ts
@@ -9,23 +9,20 @@ export class DeleteAccountComponent implements OnInit {
 
   constructor() { }
 
+  removeAccount = async () => {
+    await fetch(`http://localhost:3005/api/user`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' }
+    }).then((res) => {
+        if (res.ok) {
+            //setMessage('Account Removed');
+            return res.json();
+        }
+        throw new Error('Failed to delete account');
+    })
+  }
+  
   ngOnInit(): void {
-
-const DeleteAccount = () => {
-
-    const removeAccount = async () => {
-        await fetch(`http://localhost:3005/api/user`, {
-            method: 'DELETE',
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' }
-        }).then((res) => {
-            if (res.ok) {
-                //setMessage('Account Removed');
-                return res.json();
-            }
-            throw new Error('Failed to delete account');
-        })
-      }
-    }
   }
 }
