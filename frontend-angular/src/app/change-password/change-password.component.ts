5a38fab166667b3f04696e97cc1d0a3defc0ec63
RUnroe <ryanunroe@gmail.com>
2022-03-02 09:16:25 -0700

    Change login form query and add validation

--- frontend-angular/src/app/change-password/change-password.component.ts
+++ frontend-angular/src/app/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
 
 @Component({
   selector: 'app-change-password',
@@ -7,23 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
   newPassword: string = "";
-  constructor() { }
+  message: string = "";
+  messageClass: string = "";
+
+  constructor(private apollo: Apollo) { }
+
   changePassword = async () => {
-    await fetch(`http://localhost:3005/api/user`, {
-        method: 'PUT',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            //password: newPassword
-        })
-    }).then((res) => {
-        if (res.ok) {
-            return res.json();
-        }
-        throw new Error('Failed to update password');
-    })
+    if(this.newPassword.length >= 6) { 
+      console.log(this.newPassword);
+      this.apollo
+          .watchQuery({
+            query: gql`
+              {
+                updatePassword(password: ${this.newPassword})
+              }
+            `,
+          })
+          .valueChanges.subscribe((result: any) => {
+            console.log(result);
+            //TODO display message
+            this.messageClass = "positive";
+            this.message = "Successfully changed password!"
+          });
+    }
+    else {
+      //TODO display error
+      this.messageClass = "delete-message";
+      this.message = "Error when changing password"
+    }
+
+
+    // await fetch(`http://localhost:3005/api/user`, {
+    //     method: 'PUT',
+    //     credentials: 'include',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //         //password: newPassword
+    //     })
+    // }).then((res) => {
+    //     if (res.ok) {
+    //         return res.json();
+    //     }
+    //     throw new Error('Failed to update password');
+    // })
   }
   ngOnInit(): void {
+
   }
 
 }
