af1bc132e0a2739fa03468c9b4bf511c39660d11
RUnroe <ryanunroe@gmail.com>
2022-03-08 16:07:22 -0700

    Fix change password query

--- frontend-angular/src/app/change-password/change-password.component.ts
+++ frontend-angular/src/app/change-password/change-password.component.ts
@@ -14,43 +14,49 @@ export class ChangePasswordComponent implements OnInit {
   constructor(private apollo: Apollo) { }
   //TODO Test this error
   changePassword = async () => {
-    if(this.newPassword.length >= 6) { 
-      // console.log(this.newPassword);
-      this.apollo
-          .watchQuery({
-            query: gql`
-              {
-                updatePassword(password: ${this.newPassword})
-              }
-            `,
-          })
-          .valueChanges.subscribe((result: any) => {
-            console.log(result);
-            //Display message
-            this.messageClass = "positive";
-            this.message = "Successfully changed password!"
-          });
-    }
-    else {
-      //Display error
-      this.messageClass = "delete-message";
-      this.message = "Error when changing password"
-    }
+    // if(this.newPassword.length >= 6) { 
+    //   // console.log(this.newPassword);
+    //   this.apollo
+    //       .mutate({
+    //         mutation: gql`
+    //           {
+    //             updatePassword(password: ${this.newPassword})
+    //           }
+    //         `,
+    //       })
+    //       .subscribe((result: any) => {
+    //         console.log(result);
+    //         //Display message
+    //         this.messageClass = "positive";
+    //         this.message = "Successfully changed password!"
+    //       });
+    // }
+    // else {
+    //   //Display error
+    //   this.messageClass = "delete-message";
+    //   this.message = "Error when changing password"
+    // }
 
-
-    // await fetch(`http://localhost:3005/api/user`, {
-    //     method: 'PUT',
-    //     credentials: 'include',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify({
-    //         //password: newPassword
-    //     })
-    // }).then((res) => {
-    //     if (res.ok) {
-    //         return res.json();
-    //     }
-    //     throw new Error('Failed to update password');
-    // })
+if(this.newPassword.length >= 6) { 
+    await fetch(`http://localhost:3005/api/user`, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            password: this.newPassword
+        })
+    }).then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error('Failed to update password');
+    })
+      }
+  else {
+    //Display error
+    this.messageClass = "delete-message";
+    this.message = "Error when changing password"
+  }
   }
   ngOnInit(): void {
 
